f15d459b60a67fee8c7e9946bc5817d875412df2
Orlewilson Bentes Maia <orlewilson.maia@gmail.com>
2024-09-10 23:36:05 -0400

    Adição do exemplo de configuração do ambiente
    Signed-off-by: Orlewilson Bentes Maia <orlewilson.maia@gmail.com>

---
+++ 00_ambiente/create_image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Criar imagem
+docker build -t ambiente-oficina-fastapi-teste-software .
+
+# --tag , -t		Nome da imagem a ser criada
+
+# Fonte: https://docs.docker.com/engine/reference/commandline/build/
